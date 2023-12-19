e1ceae2dc43a035a45e270bac1c0467065bf78fd
oDzzy <samhassanrocks@yahoo.com>
2023-12-18 20:06:24 -0600

    menu gamemode toggle

--- scripts/initialize.gd
+++ scripts/initialize.gd
@@ -12,6 +12,7 @@ var isStarted = true
 var gameMode = 0;
 var isPlaced = false;
 var upperBodyOnly = false
+var spin = false
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -26,15 +27,17 @@ func _ready():
 
 func _process(delta):
 	if gameStarted:
-		if animating:
+		if spin:
 			$Ball.rotate_x(ballSpeed)
 		if $XROrigin3D/LeftController.global_position.distance_to($Ball.global_position) < $Ball/MeshInstance3D.mesh.radius or $XROrigin3D/RightController.global_position.distance_to($Ball.global_position) < $Ball/MeshInstance3D.mesh.radius:
 			if isStarted:
 				tween.stop()
+				
 				$AudioStreamPlayer3.play()
 				score += 1
 				$Saves.text = "Saves: " + str(score)
 				animating = false
+				spin = false
 			isStarted = false
 			if gameMode == 1:
 				await get_tree().create_timer(1.0).timeout
@@ -45,16 +48,17 @@ func _process(delta):
 func animate_ball():
 	if gameStarted and isPlaced:
 		isPlaced = false
+		animating = true
 		$AudioStreamPlayer2.play()
 		await get_tree().create_timer(1.5).timeout
+		spin = true
 		$AudioStreamPlayer5.play()
-		animating = true
 		tween = create_tween()
 		ballSpeed = randf_range (0.5,2.5)
 		tween.set_speed_scale(ballSpeed)
 		tween.connect("finished", on_tween_finished)
 		tween.tween_property($Ball, "position",get_random_target(), 1)
-	semaphore = semaphore + 1
+
 
 func set_random_start():
 	if gameStarted:
@@ -76,6 +80,7 @@ func on_tween_finished():
 	misses += 1
 	$Misses.text = "Misses: " + str(misses)	
 	animating = false
+	spin = false
 	set_random_start()
 	if gameMode == 1:
 		await get_tree().create_timer(1.5).timeout
@@ -101,13 +106,9 @@ func _on_left_controller_button_pressed(name):
 func _on_right_controller_button_pressed(name):
 	if gameMode == 0:
 		if name == "by_button":
-			if semaphore != 1:
-				pass
-			else:
+			if !animating:
 				set_random_start()
 		if name == "ax_button":		
-			if !animating and semaphore == 1:
-				semaphore = semaphore - 1
 			if !animating:
 				animate_ball()
 
@@ -130,3 +131,12 @@ func _on_button_2_pressed():
 	misses = 0
 	$Saves.text = "Saves: " + str(score)
 	$Misses.text = "Misses: " + str(misses)	
+
+
+func _on_check_button_2_toggled(button_pressed):
+
+	if button_pressed:
+		gameMode = 0
+	else:
+		gameMode = 1
+
